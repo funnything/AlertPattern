45d0aa6c32c26c59ce0a210e5b14f107ce0e9bcd
funnything <coding.while.sleep@gmail.com>
2020-01-20 23:47:37 +0900

    Init / gibo dump Windows macOS Linux Xcode Emacs Vim Swift Objective-C > .gitignore

---
+++ AlertPattern/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlertPattern
+//
+//  Created by Yosaku Toyama on 2020/01/20.
+//  Copyright © 2020 Yosaku Toyama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
